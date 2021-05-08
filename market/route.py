a2021f04d0f6914dfa964d96f1a510fa4fd93758
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-05-07 22:33:01 -0500

    refactor into market package

---
+++ market/route.py
@@ -0,0 +1,14 @@
+from flask import render_template
+
+from market import app
+from market.model import Item
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
--- run.sh
+++
@@ -1,3 +0,0 @@
-export FLASK_APP=market.py
-export FLASK_ENV=development
-flask run
