52b380d3200ea7d564b52e3105e7ed8da4213eed
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-05-09 14:37:47 -0500

    add logout and customizations

--- market/__init__.py
+++ market/__init__.py
@@ -9,5 +9,7 @@ app.config['SECRET_KEY'] = '804363329705210f9e068f04'
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 
 from market import route
--- market/route.py
+++ market/route.py
@@ -1,8 +1,8 @@
 from flask import flash, render_template, redirect, url_for
+from flask_login import login_user, logout_user, login_required
 
 from market import app, db
 from market.form import LoginForm, RegisterForm
-from flask_login import login_user
 from market.model import Item, User
 
 @app.route('/')
@@ -11,6 +11,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -24,6 +25,8 @@ def register_page():
             password=form.password1.data)
         db.session.add(user)
         db.session.commit()
+        login_user(user)
+        flash(f'Account created successfully! You are logged in as: {user.username}', category='success')
         return redirect(url_for('market_page'))
     if form.errors:
         for err in form.errors.values():
@@ -42,3 +45,9 @@ def login_page():
         else:
             flash('User name and password are not match!', category='danger')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('You have been logged out!', category='info')
+    return redirect(url_for('home_page'))
